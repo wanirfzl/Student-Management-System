cb553912ce44b46ee21cf1cffc071dab885f52ef
wanirfzl <wanirfzall@gmail.com>
2026-06-01 03:28:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const studentForm = document.getElementById("studentForm");
 const studentTable = document.getElementById("studentTable");
 const searchInput = document.getElementById("searchInput");
 
-let students = JSON.parse(localStorage.getItem("students")) || [];
+let students =
+JSON.parse(localStorage.getItem("students")) || [];
+
+let editIndex = null;
 
 displayStudents();
 
@@ -10,9 +13,14 @@ studentForm.addEventListener("submit", function(e){
 
     e.preventDefault();
 
-    const id = document.getElementById("studentId").value;
-    const name = document.getElementById("studentName").value;
-    const programme = document.getElementById("programme").value;
+    const id =
+    document.getElementById("studentId").value;
+
+    const name =
+    document.getElementById("studentName").value;
+
+    const programme =
+    document.getElementById("programme").value;
 
     const student = {
         id,
@@ -20,7 +28,20 @@ studentForm.addEventListener("submit", function(e){
         programme
     };
 
-    students.push(student);
+    if(editIndex !== null){
+
+        students[editIndex] = student;
+
+        editIndex = null;
+
+        document.getElementById("submitBtn")
+        .textContent = "Add Student";
+
+    }else{
+
+        students.push(student);
+
+    }
 
     localStorage.setItem(
         "students",
@@ -30,6 +51,7 @@ studentForm.addEventListener("submit", function(e){
     displayStudents();
 
     studentForm.reset();
+
 });
 
 function displayStudents(){
@@ -40,33 +62,76 @@ function displayStudents(){
 
         studentTable.innerHTML += `
         <tr>
+
             <td>${student.id}</td>
+
             <td>${student.name}</td>
-            <td>${student.programme}</td>
+
+            <td> ${student.programme} </td>
 
             <td>
+
                 <button
-                class="btn btn-danger btn-sm"
-                onclick="deleteStudent(${index})">
-                Delete
+                    class="btn btn-warning btn-sm me-2"
+                    onclick="editStudent(${index})">
+
+                    Edit
+
                 </button>
+
+                <button
+                    class="btn btn-danger btn-sm"
+                    onclick="deleteStudent(${index})">
+
+                    Delete
+
+                </button>
+
             </td>
+
         </tr>
         `;
+
     });
+
+    document.getElementById("totalStudents")
+    .textContent = students.length;
+
+}
+
+function editStudent(index){
+
+    document.getElementById("studentId").value =
+    students[index].id;
+
+    document.getElementById("studentName").value =
+    students[index].name;
+
+    document.getElementById("programme").value =
+    students[index].programme;
+
+    editIndex = index;
+
+    document.getElementById("submitBtn")
+    .textContent = "Update Student";
 
 }
 
 function deleteStudent(index){
 
-    students.splice(index,1);
+    if(confirm("Delete this student?")){
 
-    localStorage.setItem(
-        "students",
-        JSON.stringify(students)
-    );
+        students.splice(index,1);
 
-    displayStudents();
+        localStorage.setItem(
+            "students",
+            JSON.stringify(students)
+        );
+
+        displayStudents();
+
+    }
+
 }
 
 searchInput.addEventListener("keyup", function(){
